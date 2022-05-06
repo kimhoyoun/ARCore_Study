69d2352431ec3932037d67825c3f21390638d939
kimhoyoun <hoyoun1309@gmail.com>
2022-05-06 10:38:07 +0900

    8번째 실습

---
+++ Ex08_Image/app/src/main/java/com/example/ex08_image/MainActivity.java
@@ -0,0 +1,180 @@
+package com.example.ex08_image;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.hardware.display.DisplayManager;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Camera;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.LightEstimate;
+import com.google.ar.core.Plane;
+import com.google.ar.core.PointCloud;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
+import com.google.ar.core.Trackable;
+import com.google.ar.core.TrackingState;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+
+import java.util.Collection;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    GLSurfaceView mSurfaceView;
+    MainRenderer mRenderer;
+
+    Session mSession;
+    Config mConfig;
+
+    boolean mUserRequestedInstall = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideStatusBarAndTitleBar();
+        setContentView(R.layout.activity_main);
+
+        mSurfaceView = findViewById(R.id.glsurfaceview);
+        DisplayManager displayManager = (DisplayManager) getSystemService(DISPLAY_SERVICE);
+        if(displayManager != null){
+            displayManager.registerDisplayListener(new DisplayManager.DisplayListener() {
+                @Override
+                public void onDisplayAdded(int displayId) {
+
+                }
+
+                @Override
+                public void onDisplayRemoved(int displayId) {
+
+                }
+
+                @Override
+                public void onDisplayChanged(int displayId) {
+                    synchronized(this){
+                        mRenderer.mViewportChanged = true;
+                    }
+                }
+            },null);
+        }
+
+        mRenderer = new MainRenderer(this, new MainRenderer.RenderCallback() {
+            @Override
+            public void preRender() {
+                if(mRenderer.mViewportChanged){
+                    Display display = getWindowManager().getDefaultDisplay();
+                    int displayRotation = display.getRotation();
+                    mRenderer.updateSession(mSession, displayRotation);
+                }
+
+                mSession.setCameraTextureName(mRenderer.getTextureId());
+
+                Frame frame = null;
+
+                try{
+                    frame = mSession.update();
+                } catch (CameraNotAvailableException e){
+                    e.printStackTrace();
+                }
+
+                if(frame.hasDisplayGeometryChanged()){
+                    mRenderer.mCamera.transformDisplayGeometry(frame);
+                }
+
+//                mRenderer.mObj.setModelMatrix(modelMatrix);
+
+                Camera camera = frame.getCamera();
+                float [] projMatrix = new float[16];
+                camera.getProjectionMatrix(projMatrix, 0, 0.1f, 100f);
+                float[] viewMatrix = new float[16];
+                camera.getViewMatrix(viewMatrix, 0);
+
+                mRenderer.setProjectionMatrix(projMatrix);
+                mRenderer.updateViewMatrix(viewMatrix);
+
+            }
+        });
+
+        mSurfaceView.setPreserveEGLContextOnPause(true);
+        mSurfaceView.setEGLContextClientVersion(2);
+        mSurfaceView.setEGLConfigChooser(8,8,8,8,16,0);
+        mSurfaceView.setRenderer(mRenderer);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestCameraPermission();
+        try {
+            if(mSession == null){
+                switch(ArCoreApk.getInstance().requestInstall(this, true)){
+                    case INSTALLED:
+                        mSession = new Session(this);
+                        Log.d("메인", "ARCore session 생성");
+                        break;
+                    case INSTALL_REQUESTED:
+                        Log.d("메인","ARCore 설치 필요");
+                        mUserRequestedInstall = false;
+                        break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        mConfig = new Config(mSession);
+        mSession.configure(mConfig);
+        try {
+            mSession.resume();
+        } catch (CameraNotAvailableException e) {
+            e.printStackTrace();
+        }
+        mSurfaceView.onResume();
+        mSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSurfaceView.onPause();
+        mSession.pause();
+
+    }
+
+    void hideStatusBarAndTitleBar(){
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    void requestCameraPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.CAMERA},
+                    0
+            );
+        }
+    }
+
+}
